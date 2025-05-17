bed1f0a8a15282a7d6a67e316d5f56182f7cfdc4
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 16:23:50 +0200

    Añadir propiedades y metodos clase Muggle

--- src/Muggle.java
+++ src/Muggle.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class Muggle {
+public class Muggle extends Persoa{
+    private boolean crearMaxia;
+
+    public Muggle(String nome, LocalDate fechaNacimiento) {
+        super(nome, fechaNacimiento);
+        crearMaxia = false;
+    }
+
+    public Muggle(String nome, LocalDate fechaNacimiento, boolean crearMaxia) {
+        super(nome, fechaNacimiento);
+        this.crearMaxia = crearMaxia;
+    }
+
+    @Override
+    public boolean eMaiorIdade() {
+        if (calcularIdade() > 18) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Muggle{" +
+                "crearMaxia=" + crearMaxia +
+                '}';
+    }
 }
