2ca66a6aab95c0825b45801a7dd79eb001ab6ab9
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 16:16:28 +0200

    añadir Persoa2

--- src/Persoa.java
+++ src/Persoa.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.Period;
 
-public class Persoa {
+public abstract class Persoa {
+    private String nome;
+    private LocalDate fechaNacimiento;
+
+    public Persoa(String nome, LocalDate fechaNacimiento) {
+        this.nome = nome;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    abstract boolean eMaiorIdade();
+
+    protected int calcularIdade(){
+        Period periodo = Period.between(fechaNacimiento, LocalDate.now());
+        return periodo.getYears();
+    }
+
+    @Override
+    public String toString() {
+        return "Persoa{" +
+                "nome='" + nome + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                '}';
+    }
 }
