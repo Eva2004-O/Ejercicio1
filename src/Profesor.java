fe52111b9e5a2c11d0dc77531aa5f299c50a7ecc
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 18:53:10 +0200

    Añadir Profesor

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.Random;
 
-public class Profesor {
+public class Profesor extends Mago implements Docente {
+    private static final String[] frases = new String[]{"Non é bo deixarse arrastrar polos soños e esquecer vivir", "A valentía sempre é a mellor opción"};
+
+    public Profesor(String nome, LocalDate fechaNacimiento, String tipoVasoira, Casa casa) {
+        super(nome, fechaNacimiento, tipoVasoira, casa);
+    }
+
+    @Override
+    public void imponerDisciplina() {
+        System.out.println("Silencio Totalis");
+    }
+
+    @Override
+    public void disciplinar() {
+        Random r1 = new Random();
+        int fraseEscogida = r1.nextInt(frases.length);
+        System.out.println(frases[fraseEscogida]);
+    }
 }
