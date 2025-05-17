be4dfc024c79219a3073218eb8d01e245f7da925
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 16:44:57 +0200

    Crear mago y mugle

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         LocalDate fecha1 = LocalDate.of(2000, 12, 13);
         LocalDate fecha2 = LocalDate.of(2010, 5, 30);
-        Muggle m1 = new Muggle("Veron", fecha1);
-        Muggle m2 = new Muggle("Petunia", fecha2, true);
-        System.out.println(m1.eMaiorIdade());
-        System.out.println(m2);
+        Muggle mu1 = new Muggle("Veron", fecha1);
+        Muggle mu2 = new Muggle("Petunia", fecha2, true);
+        Mago ma1 = new Mago("Harry", fecha2, "Saeta de Fuego", Casa.GRYFFINDOR);
+        System.out.println(mu1.eMaiorIdade());
+        System.out.println(mu2);
+        System.out.println(ma1);
+        System.out.println(ma1.eMaiorIdade());
     }
 }
